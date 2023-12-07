05631a2f94b5a705a800995b9959cf67216c8686
Bisquitman <lynx8309@gmail.com>
2023-12-07 21:52:28 +0300

    adaptive, scrollbar for thumbs slider

--- js/index.js
+++ js/index.js
@@ -1,21 +1,48 @@
-const sliderThumbs = new Swiper(".gift__slider_thumbs", {
-  spaceBetween: 16,
-  slidesPerView: 6,
-  freeMode: true,
-  // breakpoints: {
-  //   768: {
-  //     slidesPerView: 5,
-  //   },
-  //   1024: {
-  //     slidesPerView: 6,
-  //   },
-  // }
-});
+try {
+  const sliderThumbs = new Swiper(".gift__slider_thumbs", {
+    spaceBetween: 12,
+    slidesPerView: 4,
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+      draggable: true,
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 12,
+        slidesPerView: 4,
+      },
+      768: {
+        spaceBetween: 12,
+        slidesPerView: 5,
+      },
+      1024: {
+        spaceBetween: 16,
+        slidesPerView: 6,
+      },
+    },
+  });
 
-const sliderCard = new Swiper(".gift__slider_card", {
-  spaceBetween: 16,
-  speed: 800,
-  thumbs: {
-    swiper: sliderThumbs,
-  },
-});
+  const sliderCard = new Swiper(".gift__slider_card", {
+    spaceBetween: 16,
+    speed: 800,
+    thumbs: {
+      swiper: sliderThumbs,
+    },
+  });
+} catch (error) {
+  console.log("На этой странице нет слайдеров");
+}
+
+try {
+  const card = document.querySelector(".card");
+  const cardInner = document.querySelector(".card__inner");
+
+  card.addEventListener("click", (e) => {
+    console.log("rotate");
+    cardInner.classList.toggle("_rotate");
+  });
+} catch (error) {
+  console.log("На этой странице нет отурыток");
+}
